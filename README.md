80c48d4b4e02dfec51eff7da28e70768b441656c
zitronenbaum <natalie_clarius@yahoo.de>
2021-10-17 23:02:05 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# tile-gaps
-KWin script to add space around windows snapped to a screen half or quarter.
+# Tile Gaps
+
+Extension for KDE's window manager to add space around windows snapped to a half or quarter of the screen.
+
+The size of the margins is configurable.
+
+![screenshot](screenshot.png)
+
+## Installation
+
+#### Method 1: via GUI
+
+1. Install the script via *Settings* > *Window Management* > *KWin Scripts* > *Get New Scripts …* > search for *Tile Gaps* > *Install*.
+2. Activate the script by selecting the checkbox in the *Tile Gaps* entry.
+
+#### Method 2: via command line
+
+```bash
+git clone https://github.com/nclarius/tile-gaps.git
+plasmapkg2 --type kwinscript -i tile-gaps
+kwriteconfig5 --file kwinrc --group Plugins --key tilegapsEnabled true
+qdbus org.kde.KWin /KWin reconfigure
+```
+
+
+
+## Configuration
+
+*Settings* > *Window Management* > *KWin Scripts* > configuration button in the *Tile Gaps* entry.
+
+If the configuration button is missing, try the following:
+
+````bash
+mkdir -p ~/.local/share/kservices5
+ln -s ~/.local/share/kwin/scripts/tilegaps/metadata.desktop ~/.local/share/kservices5/tilegaps.desktop
+````
+
+If that still doesn’t work, you can make the changes in the source code:
+
+1. Download and the code via GitHub (top right green button *Code* > *Download ZIP*) or KDE store (top right purple button *Download*) and unpack.
+2. In ` contents/code/main.js`, change the `true`/`false` values at the beginning of the file to your preferences.
+3. Recompile the script by starting a terminal window in the `tile-gaps` folder and running the command
+
+    ```bash
+    plasmapkg2 --type kwinscript -u .
+    ```
+
+4. Restart the KWin session by starting a terminal window and running the command
+
+   ````bash
+   kwin_x11 --replace &
+   ````
---
+++ contents/code/main.js
@@ -0,0 +1,196 @@
+/*
+KWin Script Tile Gaps
+(C) 2021 Natalie Clarius <natalie_clarius@yahoo.de>
+GNU General Public License v3.0
+*/
+
+///////////////////////
+// configuration
+///////////////////////
+
+const config = {
+    // desired size of the gap between a window and the top/left/right/bottom screen edge
+    gapScreen: readConfig("gapScreen", 12),
+
+    // desired size of the gap between two adjacent windows
+    gapWindow: readConfig("gapWindow", 12)
+};
+
+
+///////////////////////
+// setup
+///////////////////////
+
+// add to watchlist when client is initially present or added
+for (const client of workspace.clientList()) {
+    onAdded(client);
+}
+workspace.clientAdded.connect(onAdded);
+
+// trigger tile gaps when client is added, moved or resized
+function onAdded(client) {
+    workspace.clientAdded.connect(tileGaps);
+    client.geometryChanged.connect(tileGaps);
+    client.clientGeometryChanged.connect(tileGaps);
+    client.frameGeometryChanged.connect(tileGaps);
+    client.clientFinishUserMovedResized.connect(tileGaps);
+    client.moveResizedChanged(tileGaps);
+}
+
+///////////////////////
+// tile gaps
+///////////////////////
+
+// make tile gaps
+function tileGaps(win) {
+    // if no window is provided, default to active window
+    if (win == null) {
+        win = workspace.activeClient;
+    }
+    // don't act on non-normal windows, fullscreen windows or windows that are still undergoing geometry change
+    if (win == undefined || win == null || !win.normalWindow || win.fullscreen || win.move || win.resize) {
+        return;
+    }
+
+    // get client area for current window
+    const area = workspace.clientArea(win, win.screen, win.desktop);
+
+    // check if window already has the right geometry in order to prevent infinite reshaping
+    if ((  win.x == area.x + config.gapScreen // left edge is left
+        || win.x == area.x + 0.5 * area.width + 0.5 * config.gapWindow // left edge is half
+        || win.x + 0.5 * win.width == area.x + 0.5 * area.width) // center is center
+     && (  win.y == area.y + config.gapScreen // top edge is top
+        || win.y == area.y + 0.5 * area.height + 0.5 * config.gapWindow) // top edge is half
+     && (   win.width == 0.5 * area.width - config.gapScreen - 0.5 * config.gapWindow // width is half
+         || win.width == 0.5 * area.width) // width is half without gaps
+     && (  win.height == area.height - 2 * config.gapScreen // height is full
+        || win.height == 0.5 * area.height - config.gapScreen - 0.5 * config.gapWindow) // height is half
+       ) {
+       return;
+   }
+
+   // for each possible tile, check whether the window is currently approximately tiled there and if yes, adapt
+
+   // left half
+   if (near(win.x, area.x)
+    && near(win.y, area.y)
+    && near(win.width, 0.5 * area.width)
+    && near(win.height, area.height)) {
+        win.geometry = {
+            x: area.x + config.gapScreen,
+            y: area.y + config.gapScreen,
+            width: 0.5 * area.width - config.gapScreen - 0.5 * config.gapWindow,
+            height: area.height - 2 * config.gapScreen
+        };
+        return;
+    }
+
+    // center half
+    if (near(win.x + 0.5 * win.width, area.x + 0.5 * area.width)
+     && near(win.y, area.y)
+     && near(win.width, 0.5 * area.width)
+     && near(win.height, area.height)) {
+         win.geometry = {
+             x: area.x + 0.25 * area.width,
+             y: area.y + config.gapScreen,
+             width: 0.5 * area.width,
+             height: area.height - 2 * config.gapScreen
+         };
+         return;
+     }
+
+    // right half
+    if (near(win.x, area.x + 0.5 * area.width)
+     && near(win.y, area.y)
+     && near(win.width, 0.5 * area.width)
+     && near(win.height, area.height)) {
+         win.geometry = {
+             x: area.x + 0.5 * area.width + 0.5 * config.gapWindow,
+             y: area.y + config.gapScreen,
+             width: 0.5 * area.width - config.gapScreen - 0.5 * config.gapWindow,
+             height: area.height - 2 * config.gapScreen
+         };
+         return;
+     }
+
+     // top left quarter
+     if (near(win.x, area.x)
+      && near(win.y, area.y)
+      && near(win.width, 0.5 * area.width)
+      && near(win.height, 0.5 * area.height)) {
+          win.geometry = {
+              x: area.x + config.gapScreen,
+              y: area.y + config.gapScreen,
+              width: 0.5 * area.width - config.gapScreen -0.5 * config.gapWindow,
+              height: 0.5 * area.height - config.gapScreen - 0.5 * config.gapWindow
+          };
+          return;
+      }
+
+      // top right quarter
+      if (near(win.x, area.x + 0.5 * area.width)
+       && near(win.y, area.y)
+       && near(win.width, 0.5 * area.width)
+       && near(win.height, 0.5 * area.height)) {
+           win.geometry = {
+               x: area.x + 0.5 * area.width + 0.5 * config.gapWindow,
+               y: area.y + config.gapScreen,
+               width: 0.5 * area.width - config.gapScreen - 0.5 * config.gapWindow,
+               height: 0.5 * area.height - config.gapScreen - 0.5 * config.gapWindow
+           };
+           return;
+       }
+
+       // center quarter
+       if (near(win.x + 0.5 * win.width, area.x + 0.5 * area.width)
+        && near(win.y + 0.5 * win.height, area.y + 0.5 * area.height)
+        && near(win.width, 0.5 * area.width)
+        && near(win.height, area.height)) {
+            win.geometry = {
+                x: area.x + 0.25 * area.width,
+                y: area.y + 0.25 * area.height,
+                width: 0.5 * area.width,
+                height: area.height
+            };
+            return;
+        }
+
+       // bottom left quarter
+       if (near(win.x, area.x)
+        && near(win.y, area.y + 0.5 * area.height)
+        && near(win.width, 0.5 * area.width)
+        && near(win.height, 0.5 * area.height)) {
+            win.geometry = {
+                x: area.x + config.gapScreen,
+                y: area.y + 0.5 * area.height + 0.5 * config.gapWindow,
+                width: 0.5 * area.width - config.gapScreen - 0.5 * config.gapWindow,
+                height: 0.5 * area.height - config.gapScreen - 0.5 * config.gapWindow
+            };
+            return;
+        }
+
+        // bottom right quarter
+        if (near(win.x, area.x + 0.5 * area.width)
+         && near(win.y, area.y + 0.5 * area.height)
+         && near(win.width, 0.5 * area.width)
+         && near(win.height, 0.5 * area.height)) {
+             win.geometry = {
+                 x: area.x + 0.5 * area.width + 0.5 * config.gapWindow,
+                 y: area.y + 0.5 * area.height + 0.5 * config.gapWindow,
+                 width: 0.5 * area.width - config.gapScreen - 0.5 * config.gapWindow,
+                 height: 0.5 * area.height - config.gapScreen - 0.5 * config.gapWindow
+             };
+             return;
+         }
+}
+
+///////////////////////
+// helper functions
+///////////////////////
+
+// divergence margin within which to consider windows tiled
+const tolerance = 2 * Math.max(config.gapScreen, config.gapWindow);
+
+function near(actual, expected) {
+    return actual - expected <= tolerance && actual - expected >= - tolerance;
+}
