747516f13655f7034fffca1bad5d2af8791c9756
zitronenbaum <natalie_clarius@yahoo.de>
2021-10-21 16:00:06 +0200

    add support for 1x2 layout

--- contents/code/main.js
+++ contents/code/main.js
@@ -36,7 +36,7 @@ for (var i = 0; i < clients.length; i++) {
 }
 workspace.clientAdded.connect(onAdded);
 
-// trigger tile gaps when client is added, moved or resized
+// trigger tile gap when client is added, moved or resized
 function onAdded(client) {
     workspace.clientAdded.connect(tileGaps);
     client.geometryChanged.connect(tileGaps);
@@ -64,18 +64,57 @@ function tileGaps(win) {
 
     // get client area for current window
     const area = workspace.clientArea(win, win.screen, win.desktop);
-    debug("area", win.caption, win.x, win.y, win.width, win.height);
+    debug("area", area.x, area.y, area.width, area.height);
+
+    // get coordinates for area without gaps
+    dimU = {
+        // x
+        left: area.x,
+        halfHor: area.x + area.width/4,
+        right: area.x + area.width/2,
+        // y
+        top: area.y,
+        halfVert: area.y + area.height/4,
+        bottom: area.y + area.height/2,
+        // width
+        fullWidth: area.width,
+        halfWidth: area.width/2,
+        // height
+        fullHeight: area.height,
+        halfHeight: area.height/2
+    };
+
+    // get coordinates for area with gaps
+    dimG = {
+        // x
+        left: dimU.left + config.gapScreen,
+        halfHor: dimU.halfHor,
+        right: dimU.right + config.gapWindow/2,
+        // y
+        top: dimU.top + config.gapScreen,
+        halfVert: dimU.halfVert,
+        bottom: dimU.bottom + config.gapWindow/2,
+        // width
+        fullWidth: dimU.fullWidth - config.gapScreen*2,
+        halfWidth: dimU.halfWidth,
+        // height
+        fullHeight: dimU.fullHeight - config.gapScreen*2,
+        halfHeight: dimU.halfHeight
+    };
 
     // check if window already has the right geometry in order to prevent infinite reshaping
     if ((  win.x == area.x + config.gapScreen // left edge is left
-        || win.x == area.x + 0.5 * area.width + 0.5 * config.gapWindow // left edge is half
-        || win.x + 0.5 * win.width == area.x + 0.5 * area.width) // center is center
+        || win.x == area.x + area.width/2 + config.gapWindow/2 // left edge is half
+        || win.x == area.x + area.width/4) // left edge is quarter
      && (  win.y == area.y + config.gapScreen // top edge is top
-        || win.y == area.y + 0.5 * area.height + 0.5 * config.gapWindow) // top edge is half
-     && (   win.width == 0.5 * area.width - config.gapScreen - 0.5 * config.gapWindow // width is half
-         || win.width == 0.5 * area.width) // width is half without gaps
-     && (  win.height == area.height - 2 * config.gapScreen // height is full
-        || win.height == 0.5 * area.height - config.gapScreen - 0.5 * config.gapWindow) // height is half
+        || win.y == area.y + area.height/2 + config.gapWindow/2 // top edge is half
+        || win.y == area.y + area.height/4) // top edge is quarter
+     && (  win.width == area.width - config.gapScreen*2 // width is full
+         || win.width == area.width/2 - config.gapScreen - config.gapWindow/2 // width is half
+         || win.width == area.width/2) // width is half w/o gaps
+     && (  win.height == area.height - config.gapScreen*2 // height is full
+        || win.height == area.height/2 - config.gapScreen - config.gapWindow/2 // height is half
+        || win.height == area.height/2) // height is half w/o gaps
        ) {
            debug("gapped");
        return;
@@ -86,82 +125,133 @@ function tileGaps(win) {
    // left half
    if (near(win.x, area.x)
     && near(win.y, area.y)
-    && near(win.width, 0.5 * area.width)
+    && near(win.width, area.width/2)
     && near(win.height, area.height)) {
+        debug("gap left");
         win.geometry = {
             x: area.x + config.gapScreen,
             y: area.y + config.gapScreen,
-            width: 0.5 * area.width - config.gapScreen - 0.5 * config.gapWindow,
-            height: area.height - 2 * config.gapScreen
+            width: area.width/2 - config.gapScreen - config.gapWindow/2,
+            height: area.height - config.gapScreen*2
         };
         return;
     }
 
-    // center half
-    if (near(win.x + 0.5 * win.width, area.x + 0.5 * area.width)
+    // horizontal center half
+    if (near(win.x, area.y + area.width/4)
      && near(win.y, area.y)
-     && near(win.width, 0.5 * area.width)
+     && near(win.width, area.width/2)
      && near(win.height, area.height)) {
+         debug("gap center horizontal");
          win.geometry = {
-             x: area.x + 0.25 * area.width,
+             x: area.x + area.width/4,
              y: area.y + config.gapScreen,
-             width: 0.5 * area.width,
-             height: area.height - 2 * config.gapScreen
+             width: area.width/2,
+             height: area.height - config.gapScreen*2
          };
          return;
      }
 
     // right half
-    if (near(win.x, area.x + 0.5 * area.width)
+    if (near(win.x, area.x + area.width/2)
      && near(win.y, area.y)
-     && near(win.width, 0.5 * area.width)
+     && near(win.width, area.width/2)
      && near(win.height, area.height)) {
+         debug("gap right");
          win.geometry = {
-             x: area.x + 0.5 * area.width + 0.5 * config.gapWindow,
+             x: area.x + area.width/2 + config.gapWindow/2,
              y: area.y + config.gapScreen,
-             width: 0.5 * area.width - config.gapScreen - 0.5 * config.gapWindow,
-             height: area.height - 2 * config.gapScreen
+             width: area.width/2 - config.gapScreen - config.gapWindow/2,
+             height: area.height - config.gapScreen*2
          };
          return;
      }
 
-     // top left quarter
+     // top half
      if (near(win.x, area.x)
       && near(win.y, area.y)
-      && near(win.width, 0.5 * area.width)
-      && near(win.height, 0.5 * area.height)) {
+      && near(win.width, area.width)
+      && near(win.height, area.height/2)) {
+          debug("gap top");
           win.geometry = {
               x: area.x + config.gapScreen,
               y: area.y + config.gapScreen,
-              width: 0.5 * area.width - config.gapScreen -0.5 * config.gapWindow,
-              height: 0.5 * area.height - config.gapScreen - 0.5 * config.gapWindow
+              width: area.width - config.gapScreen*2,
+              height: area.height/2 - config.gapScreen - config.gapWindow/2
+          };
+          return;
+      }
+
+      // vertical center half
+      if (near(win.x, area.x)
+       && near(win.y, area.y)
+       && near(win.width, area.width)
+       && near(win.height, area.height/2)) {
+           debug("gap center vertical");
+           win.geometry = {
+               x: area.x + config.gapScreen,
+               y: area.y + area.height/4,
+               width: area.width - config.gapScreen*2,
+               height: area.height/2
+           };
+           return;
+       }
+
+       // bottom half
+       if (near(win.x, area.x)
+        && near(win.y, area.y + area.height/2)
+        && near(win.width, area.width)
+        && near(win.height, area.height/2)) {
+            debug("gap bottom");
+            win.geometry = {
+                x: area.x + config.gapScreen,
+                y: area.y + area.height/2 + config.gapWindow/2,
+                width: area.width - config.gapScreen*2,
+                height: area.height/2 - config.gapScreen - config.gapWindow/2
+            };
+            return;
+        }
+
+     // top left quarter
+     if (near(win.x, area.x)
+      && near(win.y, area.y)
+      && near(win.width, area.width/2)
+      && near(win.height, area.height/2)) {
+          debug("gap top left");
+          win.geometry = {
+              x: area.x + config.gapScreen,
+              y: area.y + config.gapScreen,
+              width: area.width/2 - config.gapScreen -config.gapWindow/2,
+              height: area.height/2 - config.gapScreen - config.gapWindow/2
           };
           return;
       }
 
       // top right quarter
-      if (near(win.x, area.x + 0.5 * area.width)
+      if (near(win.x, area.x + area.width/2)
        && near(win.y, area.y)
-       && near(win.width, 0.5 * area.width)
-       && near(win.height, 0.5 * area.height)) {
+       && near(win.width, area.width/2)
+       && near(win.height, area.height/2)) {
+           debug("gap top right");
            win.geometry = {
-               x: area.x + 0.5 * area.width + 0.5 * config.gapWindow,
+               x: area.x + area.width/2 + config.gapWindow/2,
                y: area.y + config.gapScreen,
-               width: 0.5 * area.width - config.gapScreen - 0.5 * config.gapWindow,
-               height: 0.5 * area.height - config.gapScreen - 0.5 * config.gapWindow
+               width: area.width/2 - config.gapScreen - config.gapWindow/2,
+               height: area.height/2 - config.gapScreen - config.gapWindow/2
            };
            return;
        }
 
        // center quarter
-       if (near(win.x + 0.5 * win.width, area.x + 0.5 * area.width)
-        && near(win.y + 0.5 * win.height, area.y + 0.5 * area.height)
-        && near(win.width, 0.5 * area.width)
+       if (near(win.x + 0.5 * win.width, area.x + area.width/2)
+        && near(win.y + 0.5 * win.height, area.y + area.height/2)
+        && near(win.width, area.width/2)
         && near(win.height, area.height)) {
+            debug("gap center quarter");
             win.geometry = {
-                x: area.x + 0.25 * area.width,
-                y: area.y + 0.25 * area.height,
-                width: 0.5 * area.width,
+                x: area.x + area.width/4,
+                y: area.y + area.height/4,
+                width: area.width/2,
                 height: area.height
             };
             return;
@@ -169,28 +259,30 @@ function tileGaps(win) {
 
        // bottom left quarter
        if (near(win.x, area.x)
-        && near(win.y, area.y + 0.5 * area.height)
-        && near(win.width, 0.5 * area.width)
-        && near(win.height, 0.5 * area.height)) {
+        && near(win.y, area.y + area.height/2)
+        && near(win.width, area.width/2)
+        && near(win.height, area.height/2)) {
+            debug("gap bottom left");
             win.geometry = {
                 x: area.x + config.gapScreen,
-                y: area.y + 0.5 * area.height + 0.5 * config.gapWindow,
-                width: 0.5 * area.width - config.gapScreen - 0.5 * config.gapWindow,
-                height: 0.5 * area.height - config.gapScreen - 0.5 * config.gapWindow
+                y: area.y + area.height/2 + config.gapWindow/2,
+                width: area.width/2 - config.gapScreen - config.gapWindow/2,
+                height: area.height/2 - config.gapScreen - config.gapWindow/2
             };
             return;
         }
 
         // bottom right quarter
-        if (near(win.x, area.x + 0.5 * area.width)
-         && near(win.y, area.y + 0.5 * area.height)
-         && near(win.width, 0.5 * area.width)
-         && near(win.height, 0.5 * area.height)) {
+        if (near(win.x, area.x + area.width/2)
+         && near(win.y, area.y + area.height/2)
+         && near(win.width, area.width/2)
+         && near(win.height, area.height/2)) {
+             debug("gap bottom right");
              win.geometry = {
-                 x: area.x + 0.5 * area.width + 0.5 * config.gapWindow,
-                 y: area.y + 0.5 * area.height + 0.5 * config.gapWindow,
-                 width: 0.5 * area.width - config.gapScreen - 0.5 * config.gapWindow,
-                 height: 0.5 * area.height - config.gapScreen - 0.5 * config.gapWindow
+                 x: area.x + area.width/2 + config.gapWindow/2,
+                 y: area.y + area.height/2 + config.gapWindow/2,
+                 width: area.width/2 - config.gapScreen - config.gapWindow/2,
+                 height: area.height/2 - config.gapScreen - config.gapWindow/2
              };
              return;
          }
