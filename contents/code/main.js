2cb661d42595fe5851d72b463ef3591c91e684f5
zitronenbaum <natalie_clarius@yahoo.de>
2021-10-22 17:37:18 +0200

    fix script not working on startup

--- contents/code/main.js
+++ contents/code/main.js
@@ -4,13 +4,6 @@ KWin Script Tile Gaps
 GNU General Public License v3.0
 */
 
-///////////////////////
-// initialization
-///////////////////////
-debugMode = true;
-function debug(...args) {if (debugMode) {console.debug(...args);}}
-debug("tile gaps");
-
 
 ///////////////////////
 // configuration
@@ -23,54 +16,60 @@ const config = {
     // desired size of the gap between two adjacent windows
     gapWindow: readConfig("gapWindow", 12)
 };
-debug("gap size screen:", config.gapScreen, "gap size window:", config.gapWindow);
+
+
+///////////////////////
+// initialization
+///////////////////////
+
+debugMode = true;
+function debug(...args) {if (debugMode) {console.debug(...args);}}
+debug("intializing tile gaps");
+debug("tile gap settings:", "gap size screen:", config.gapScreen, "gap size window:", config.gapWindow);
+
+
+///////////////////////
+// set up triggers
+///////////////////////
+
+// add to watchlist when client is initially present or added
+const clients = workspace.clientList();
+for (var i = 0; i < clients.length; i++) {
+    onAdded(clients[i]);
+}
+workspace.clientAdded.connect(onAdded);
+
+// trigger tile gap when client is added, moved or resized
+function onAdded(client) {
+    workspace.clientAdded.connect(tileGaps);
+    client.geometryChanged.connect(tileGaps);
+    client.clientGeometryChanged.connect(tileGaps);
+    client.frameGeometryChanged.connect(tileGaps);
+    client.clientFinishUserMovedResized.connect(tileGaps);
+    client.moveResizedChanged.connect(tileGaps);
+}
 
 
 ///////////////////////
 // get area geometry
 ///////////////////////
 
-const clients = workspace.clientList();
-const areas = {};
-const grids = {};
-const tiless = {};
-
-// get geometry for all screens initially and whenever screen setup changes
-updateGeometry();
-workspace.numberScreensChanged.connect(updateGeometry);
-workspace.screenResized.connect(updateGeometry);
-
-// update geometry of all screens
-function updateGeometry() {
-    // find desktop background for each screen
-    for (var i = 0; i < clients.length; i++) {
-        client = clients[i];
-        if (client.desktopWindow) {
-            screenNr = client.screen;
-            // update area, grid and tiles for screen
-            areas[screenNr] = getArea(client);
-            grids[screenNr] = getGrid(client);
-            tiless[screenNr] = getTiles(client);
-        }
-    }
-}
-
 // client area
 function getArea(client) {
     return workspace.clientArea(client, client.screen, client.desktop);
 }
 
-// area anchors
+// anchor coordinates with gaps
 function getGrid(client) {
-    const area = areas[client.screen];
+    var area = getArea(client);
     return {
         // x
         left: area.x + config.gapScreen,
-        midH: area.x + (config.gapScreen - config.gapWindow/2)/2,
+        midH: area.x + area.width/4 + (config.gapScreen + config.gapWindow/2)/2,
         right: area.x + area.width/2 + config.gapWindow/2,
         // y
         top: area.y + config.gapScreen,
-        midV: area.y + (config.gapScreen - config.gapWindow/2)/2,
+        midV: area.y + area.height/4 + (config.gapScreen + config.gapWindow/2)/2,
         bottom: area.y + area.height/2 + config.gapWindow/2,
         // width
         fullWidth: area.width - config.gapScreen - config.gapScreen,
@@ -78,13 +77,12 @@ function getGrid(client) {
         // height
         fullHeight: area.height - config.gapScreen - config.gapScreen,
         halfHeight: area.height/2 - config.gapScreen - config.gapWindow/2
-    }
+    };
 }
 
 // coordinates for each tile
-function getTiles(screenNr) {
-    const area = areas[client.screen];
-    const grid = grids[client.screen];
+function getTiles(client) {
+    var grid = getGrid(client);
     return {
         halfLeft: {
             x: grid.left,
@@ -176,35 +174,25 @@ function getTiles(screenNr) {
             width: grid.halfWidth,
             height: grid.halfHeight
         }
-    }
+    };
 }
 
-///////////////////////
-// triggers
-///////////////////////
-
-// add to watchlist when client is initially present or added
-for (var i = 0; i < clients.length; i++) {
-    onAdded(clients[i]);
-}
-workspace.clientAdded.connect(onAdded);
-
-// trigger tile gap when client is added, moved or resized
-function onAdded(client) {
-    workspace.clientAdded.connect(tileGaps);
-    client.geometryChanged.connect(tileGaps);
-    client.clientGeometryChanged.connect(tileGaps);
-    client.frameGeometryChanged.connect(tileGaps);
-    client.clientFinishUserMovedResized.connect(tileGaps);
-    client.moveResizedChanged(tileGaps);
+// window is considered tiled iff the difference on all coordinates between actual and expected geometry is within the tolerated divergence margin
+const tolerance = 2 * Math.max(config.gapScreen, config.gapWindow);
+function near(actual, expected) {
+    return Object.keys(expected).every(coord =>
+        actual[coord] - expected[coord] >= -tolerance
+     && actual[coord] - expected[coord] <=  tolerance);
 }
 
+
 ///////////////////////
-// create tile gaps
+// make tile gaps
 ///////////////////////
 
+// make tile gaps for given window
 function tileGaps(win) {
-    // get window to gap
+    // get window to be gapped
     // if no window is provided, default to active window
     if (win == null) {
         win = workspace.activeClient;
@@ -213,36 +201,23 @@ function tileGaps(win) {
     if (win == undefined || win == null || !win.normalWindow || win.fullscreen || win.move || win.resize) {
         return;
     }
-    area = areas[win.screen];
     debug("gaps for", win.caption);
-    debug("window geometry", win.x, win.y, win.width, win.height);
-    debug("area geometry", area.x, area.y, area.width, area.height);
+    debug("window geometry", ...Object.values(win.geometry));
 
     // iterate possible tile positions
-    tiles = tiless[win.screen];
+    var tiles = getTiles(win);
     for (var i = 0; i < Object.keys(tiles).length; i++) {
-        var tile = {name: Object.keys(tiles)[i]};
-        tile.geometry = tiles[tile.name];
+        var tile = {label: Object.keys(tiles)[i]}; // position label
+        tile.geometry = tiles[tile.label]; // geometry (with gaps)
 
         // check if the window is approximately tiled there
         if (near(win.geometry, tile.geometry)) {
             // window is tiled: apply gapped geometry
-            debug("gapping", tile.name, ...Object.values(tile.geometry), "\n");
+            debug("gapping", tile.label, ...Object.values(tile.geometry), "\n");
             win.geometry = tile.geometry;
             return;
         }
     }
+    // window's geometry doesn't approximately match any possible tile: don't gap
     debug("not tiled\n");
  }
-
-///////////////////////
-// helper functions
-///////////////////////
-
-// window is considered tiled iff the difference between actual and expected geometry for all coordinates is within tolerated divergence margin
-const tolerance = 2 * Math.max(config.gapScreen, config.gapWindow);
-function near(actual, expected) {
-    return Object.keys(expected).every(coord =>
-        actual[coord] - expected[coord] >= -tolerance
-     && actual[coord] - expected[coord] <=  tolerance);
-}
