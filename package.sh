f72a19831182f63478550dbf323730f529aeae9f
nclarius <natalie_clarius@yahoo.de>
2022-03-18 23:03:40 +0100

    fix bugs with logout and splash screens

--- package.sh
+++ package.sh
@@ -15,8 +15,8 @@ echo 'generated changelog markdown'
 # generate changelog in bbcode format
 heading_bb=$([[ $version == *.0 ]] && echo "h1" || echo "h2")
 caption_bb='['"$heading_bb"']v'"$version"'[/'"$heading_bb"']'
-changes_bb='[list]\n'"$(cat CHANGELOG.txt | sed 's/- /[*] /g')"$'\n[/list]'
-echo "$caption_bb"$'\n'"$changes_bb"$'\n\n'"$(cat CHANGELOG.bbcode)" > "CHANGELOG.bbcode"
+changes_bb='[list]\n'"$(cat CHANGELOG.txt | sed 's/- /[*] /g')"'\n[/list]'
+"$caption_bb"'\n'"$changes_bb"'\n\n'"$(cat CHANGELOG.bbcode)" > "CHANGELOG.bbcode"
 echo 'generated changelog bbcode'
 
 # generate GitHub release
